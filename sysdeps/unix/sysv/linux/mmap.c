f52c1d77826020920ef810a01f687d437f9b14c3
Yuchen Zhang <yzhang0701@gmail.com>
2024-06-12 13:40:15 -0400

    Add: handle mmap syscall

--- sysdeps/unix/sysv/linux/mmap.c
+++ sysdeps/unix/sysv/linux/mmap.c
@@ -30,21 +30,28 @@
 #  define MMAP_ADJUST_OFFSET(offset) offset
 # endif
 
+// void *
+// __mmap (void *addr, size_t len, int prot, int flags, int fd, off_t offset)
+// {
+//   MMAP_CHECK_PAGE_UNIT ();
+
+//   if (offset & MMAP_OFF_LOW_MASK)
+//     return (void *) INLINE_SYSCALL_ERROR_RETURN_VALUE (EINVAL);
+
+// #ifdef __NR_mmap2
+//   return (void *) MMAP_CALL (mmap2, addr, len, prot, flags, fd,
+// 			     offset / (uint32_t) MMAP2_PAGE_UNIT);
+// #else
+//   return (void *) MMAP_CALL (mmap, addr, len, prot, flags, fd,
+// 			     MMAP_ADJUST_OFFSET (offset));
+// #endif
+// }
+
+// Edit: Dennis
 void *
 __mmap (void *addr, size_t len, int prot, int flags, int fd, off_t offset)
 {
-  MMAP_CHECK_PAGE_UNIT ();
-
-  if (offset & MMAP_OFF_LOW_MASK)
-    return (void *) INLINE_SYSCALL_ERROR_RETURN_VALUE (EINVAL);
-
-#ifdef __NR_mmap2
-  return (void *) MMAP_CALL (mmap2, addr, len, prot, flags, fd,
-			     offset / (uint32_t) MMAP2_PAGE_UNIT);
-#else
-  return (void *) MMAP_CALL (mmap, addr, len, prot, flags, fd,
-			     MMAP_ADJUST_OFFSET (offset));
-#endif
+  MAKE_SYSCALL(21, "syscall|mmap", (uint64_t)(uintptr_t) addr, (uint64_t) len, (uint64_t) prot, (uint64_t) flags, (uint64_t) fd, (uint64_t) offset);
 }
 weak_alias (__mmap, mmap)
 libc_hidden_def (__mmap)

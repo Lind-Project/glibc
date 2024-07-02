3a3c46ff63eac5956cf581d313b27997338ff6db
Yuchen Zhang <yzhang0701@gmail.com>
2024-07-02 10:32:56 -0400

    Mod: implement BYTE_COPY_FWD and BYTE_COPY_BWD in c

--- sysdeps/i386/memcopy.h
+++ sysdeps/i386/memcopy.h
@@ -20,40 +20,61 @@
 #include <string.h>  // For memcpy
 
 #undef BYTE_COPY_FWD
-#define BYTE_COPY_FWD(dst_bp, src_bp, nbytes)    \
-  do {                                           \
-    memcpy(dst_bp, src_bp, nbytes);              \
-    dst_bp += nbytes;                            \
-    src_bp += nbytes;                            \
-  } while (0)
+#define BYTE_COPY_FWD(dst_bp, src_bp, nbytes)				      \
+  do									      \
+    {									      \
+      size_t __nbytes = (nbytes);					      \
+      if (__nbytes & 1)							      \
+        {								      \
+	  ((byte *) dst_bp)[0] =  ((byte *) src_bp)[0];			      \
+	  src_bp += 1;							      \
+	  dst_bp += 1;							      \
+	  __nbytes -= 1;						      \
+        }								      \
+      while (__nbytes > 0)						      \
+	{								      \
+	  byte __x = ((byte *) src_bp)[0];				      \
+	  byte __y = ((byte *) src_bp)[1];				      \
+	  src_bp += 2;							      \
+	  __nbytes -= 2;						      \
+	  ((byte *) dst_bp)[0] = __x;					      \
+	  ((byte *) dst_bp)[1] = __y;					      \
+	  dst_bp += 2;							      \
+	}								      \
+    } while (0)
 
+/* Copy exactly NBYTES_TO_COPY bytes from SRC_END_PTR to DST_END_PTR,
+   beginning at the bytes right before the pointers and continuing towards
+   smaller addresses.  Don't assume anything about alignment of the
+   pointers.  */
 #undef BYTE_COPY_BWD
-#define BYTE_COPY_BWD(dst_ep, src_ep, nbytes)         \
-  do                                                  \
-    {                                                 \
-      memmove((dst_ep) - (nbytes),                    \
-              (src_ep) - (nbytes),                    \
-              nbytes);                                \
-      dst_ep += 1;                                    \
-      src_ep += 1;                                    \
+#define BYTE_COPY_BWD(dst_ep, src_ep, nbytes)				      \
+  do									      \
+    {									      \
+      size_t __nbytes = (nbytes);					      \
+      if (__nbytes & 1)							      \
+        {								      \
+	  src_ep -= 1;							      \
+	  dst_ep -= 1;							      \
+	  ((byte *) dst_ep)[0] =  ((byte *) src_ep)[0];			      \
+	  __nbytes -= 1;						      \
+        }								      \
+      while (__nbytes > 0)						      \
+	{								      \
+	  byte __x, __y;						      \
+	  src_ep -= 2;							      \
+	  __y = ((byte *) src_ep)[1];					      \
+	  __x = ((byte *) src_ep)[0];					      \
+	  dst_ep -= 2;							      \
+	  __nbytes -= 2;						      \
+	  ((byte *) dst_ep)[1] = __y;					      \
+	  ((byte *) dst_ep)[0] = __x;					      \
+	}								      \
     } while (0)
 
-#undef	WORD_COPY_FWD
-#define WORD_COPY_FWD(dst_bp, src_bp, nbytes_left, nbytes)            \
-  do                                                                  \
-    {                                                                 \
-      size_t __nbytes = (nbytes);                                     \
-      char *dst = (char *)(dst_bp);                                   \
-      const char *src = (const char *)(src_bp);                       \
-      /* Ensure the source and destination do not overlap */          \
-      if (dst != src && (src + __nbytes > dst || dst + __nbytes > src))  \
-      {                                                               \
-        /* Copy bytes */                                              \
-        for (size_t i = 0; i < __nbytes; i++)                         \
-          dst[i] = src[i];                                            \
-      }                                                               \
-      (nbytes_left) = 0;                                              \
-    } while (0)
+/* The powerpc memcpy implementation is safe to use for memmove.  */
+#undef MEMCPY_OK_FOR_FWD_MEMMOVE
+#define MEMCPY_OK_FOR_FWD_MEMMOVE 1
 
 #undef	WORD_COPY_BWD
 #define WORD_COPY_BWD(dst_ep, src_ep, nbytes_left, nbytes)          \

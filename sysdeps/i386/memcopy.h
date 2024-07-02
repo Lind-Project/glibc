ef2904293d476e99bf10e37c0c053836e9c9502e
Yuchen Zhang <yzhang0701@gmail.com>
2024-07-02 12:12:31 -0400

    Mod: use Generic C version for memcopy.h

--- sysdeps/i386/memcopy.h
+++ sysdeps/i386/memcopy.h
@@ -19,27 +19,19 @@
 #include <sysdeps/generic/memcopy.h>
 #include <string.h>  // For memcpy
 
+// Dennis Edit: fixing the infinite loop error in printf
 #undef BYTE_COPY_FWD
 #define BYTE_COPY_FWD(dst_bp, src_bp, nbytes)				      \
   do									      \
     {									      \
       size_t __nbytes = (nbytes);					      \
-      if (__nbytes & 1)							      \
-        {								      \
-	  ((byte *) dst_bp)[0] =  ((byte *) src_bp)[0];			      \
-	  src_bp += 1;							      \
-	  dst_bp += 1;							      \
-	  __nbytes -= 1;						      \
-        }								      \
       while (__nbytes > 0)						      \
 	{								      \
 	  byte __x = ((byte *) src_bp)[0];				      \
-	  byte __y = ((byte *) src_bp)[1];				      \
-	  src_bp += 2;							      \
-	  __nbytes -= 2;						      \
+	  src_bp += 1;							      \
+	  __nbytes -= 1;						      \
 	  ((byte *) dst_bp)[0] = __x;					      \
-	  ((byte *) dst_bp)[1] = __y;					      \
-	  dst_bp += 2;							      \
+	  dst_bp += 1;							      \
 	}								      \
     } while (0)
 
@@ -52,40 +44,48 @@
   do									      \
     {									      \
       size_t __nbytes = (nbytes);					      \
-      if (__nbytes & 1)							      \
-        {								      \
-	  src_ep -= 1;							      \
-	  dst_ep -= 1;							      \
-	  ((byte *) dst_ep)[0] =  ((byte *) src_ep)[0];			      \
-	  __nbytes -= 1;						      \
-        }								      \
       while (__nbytes > 0)						      \
 	{								      \
-	  byte __x, __y;						      \
-	  src_ep -= 2;							      \
-	  __y = ((byte *) src_ep)[1];					      \
+	  byte __x;							      \
+	  src_ep -= 1;							      \
 	  __x = ((byte *) src_ep)[0];					      \
-	  dst_ep -= 2;							      \
-	  __nbytes -= 2;						      \
-	  ((byte *) dst_ep)[1] = __y;					      \
+	  dst_ep -= 1;							      \
+	  __nbytes -= 1;						      \
 	  ((byte *) dst_ep)[0] = __x;					      \
 	}								      \
     } while (0)
 
-/* The powerpc memcpy implementation is safe to use for memmove.  */
-#undef MEMCPY_OK_FOR_FWD_MEMMOVE
-#define MEMCPY_OK_FOR_FWD_MEMMOVE 1
+extern void _wordcopy_fwd_aligned (long int, long int, size_t)
+  attribute_hidden __THROW;
+extern void _wordcopy_fwd_dest_aligned (long int, long int, size_t)
+  attribute_hidden __THROW;
+#undef	WORD_COPY_FWD
+#define WORD_COPY_FWD(dst_bp, src_bp, nbytes_left, nbytes)		      \
+  do									      \
+    {									      \
+      if (src_bp % OPSIZ == 0)						      \
+	_wordcopy_fwd_aligned (dst_bp, src_bp, (nbytes) / OPSIZ);	      \
+      else								      \
+	_wordcopy_fwd_dest_aligned (dst_bp, src_bp, (nbytes) / OPSIZ);	      \
+      src_bp += (nbytes) & -OPSIZ;					      \
+      dst_bp += (nbytes) & -OPSIZ;					      \
+      (nbytes_left) = (nbytes) % OPSIZ;					      \
+    } while (0)
 
+extern void _wordcopy_bwd_aligned (long int, long int, size_t)
+  attribute_hidden __THROW;
+extern void _wordcopy_bwd_dest_aligned (long int, long int, size_t)
+  attribute_hidden __THROW;
 #undef	WORD_COPY_BWD
-#define WORD_COPY_BWD(dst_ep, src_ep, nbytes_left, nbytes)          \
-  do                                                                \
-    {                                                               \
-      size_t __nbytes = (nbytes);                                   \
-      char *dst = (char *)(dst_ep);                                 \
-      const char *src = (const char *)(src_ep);                     \
-      /* Copy bytes backward from the end */                        \
-      for (size_t i = 0; i < __nbytes; ++i)                         \
-        dst[__nbytes - 1 - i] = src[__nbytes - 1 - i];              \
-      (nbytes_left) = 0;                                            \
+#define WORD_COPY_BWD(dst_ep, src_ep, nbytes_left, nbytes)		      \
+  do									      \
+    {									      \
+      if (src_ep % OPSIZ == 0)						      \
+	_wordcopy_bwd_aligned (dst_ep, src_ep, (nbytes) / OPSIZ);	      \
+      else								      \
+	_wordcopy_bwd_dest_aligned (dst_ep, src_ep, (nbytes) / OPSIZ);	      \
+      src_ep -= (nbytes) & -OPSIZ;					      \
+      dst_ep -= (nbytes) & -OPSIZ;					      \
+      (nbytes_left) = (nbytes) % OPSIZ;					      \
     } while (0)
 

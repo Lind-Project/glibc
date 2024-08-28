3785fc8abdd187cce93b747d69b94de8c8d85360
Runbin Yuan <144069479+robinyuan1002@users.noreply.github.com>
2024-08-28 20:55:58 +0800

    Update chdir.c

--- sysdeps/unix/sysv/linux/chdir.c
+++ sysdeps/unix/sysv/linux/chdir.c
@@ -3,11 +3,13 @@
 int
 __chdir (const char *__path)
 {
-  return 0;
+  // return 0;
+  return MAKE_SYSCALL(130, "syscall|chdir", (uint64_t) __path, NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
 }
 
 int
 chdir (const char *__path)
 {
-  return 0;
+  // return 0;
+  return MAKE_SYSCALL(130, "syscall|chdir", (uint64_t) __path, NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
 }

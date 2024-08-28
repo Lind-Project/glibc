fef1e5e9703b767010272a93378ce97ca5418892
Runbin Yuan <144069479+robinyuan1002@users.noreply.github.com>
2024-08-28 18:19:58 +0800

    Update rename.c glibc

--- sysdeps/unix/sysv/linux/rename.c
+++ sysdeps/unix/sysv/linux/rename.c
@@ -25,11 +25,12 @@
 int
 rename (const char *old, const char *new)
 {
-#if defined (__NR_rename)
-  return INLINE_SYSCALL_CALL (rename, old, new);
-#elif defined (__NR_renameat)
-  return INLINE_SYSCALL_CALL (renameat, AT_FDCWD, old, AT_FDCWD, new);
-#else
-  return INLINE_SYSCALL_CALL (renameat2, AT_FDCWD, old, AT_FDCWD, new, 0);
-#endif
+// #if defined (__NR_rename)
+//   return INLINE_SYSCALL_CALL (rename, old, new);
+// #elif defined (__NR_renameat)
+//   return INLINE_SYSCALL_CALL (renameat, AT_FDCWD, old, AT_FDCWD, new);
+// #else
+//   return INLINE_SYSCALL_CALL (renameat2, AT_FDCWD, old, AT_FDCWD, new, 0);
+// #endif
+   return MAKE_SYSCALL(6, "syscall|rename", (uint64_t) old, (uint64_t) new, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
 }

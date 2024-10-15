bd8c0374fb198dc9c206198892d212ebf35d6914
Yashaswi Makula <yashaswi.makula@gmail.com>
2024-10-15 00:05:06 -0400

    stable-changes

--- wasm-config.sh
+++ wasm-config.sh
@@ -6,5 +6,5 @@ BUILDDIR=build
 mkdir -p $BUILDDIR
 cd $BUILDDIR
 ../configure --disable-werror --disable-hidden-plt --disable-profile --with-headers=/usr/i686-linux-gnu/include --prefix=/home/dennis/Documents/lind-wasm/glibc/target --host=i686-linux-gnu --build=i686-linux-gnu\
-    CFLAGS="-mbulk-memory -O2 -g" \
-    CC="/home/dennis/Documents/Just-One-Turtle/clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04/bin/clang --target=wasm32-unkown-wasi -v -Wno-int-conversion"
+    CFLAGS="-matomics -mbulk-memory -O2 -g" \
+    CC="/home/clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04/bin/clang --target=wasm32-unkown-wasi -v -Wno-int-conversion"
